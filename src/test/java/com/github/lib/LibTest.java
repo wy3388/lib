65df31f8c85238880d24d660d2e4120cf6b468f2
wy <wy.pp@foxmail.com>
2022-01-12 10:18:30 +0800

    init

---
+++ src/test/java/com/github/lib/LibTest.java
@@ -0,0 +1,16 @@
+package com.github.lib;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created on 2022/1/12.
+ *
+ * @author wy
+ */
+public class LibTest {
+
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+}
