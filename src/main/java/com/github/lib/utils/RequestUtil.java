cf83746aa3e7003a4a26b4c0b4497cf6b713ff4e
wy <wy.pp@foxmail.com>
2022-01-13 10:04:55 +0800

    删除依赖

--- src/main/java/com/github/lib/utils/RequestUtil.java
+++ src/main/java/com/github/lib/utils/RequestUtil.java
@@ -1,17 +1,11 @@
 package com.github.lib.utils;
 
 import lombok.NonNull;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created on 2022/1/12.
@@ -29,30 +23,6 @@ public final class RequestUtil {
      */
     @NonNull
     public String get(@NonNull final String url, final Map<String, String> headerMap) {
-        OkHttpClient client = new OkHttpClient.Builder()
-                .connectTimeout(10, TimeUnit.SECONDS)
-                .readTimeout(20, TimeUnit.SECONDS)
-                .writeTimeout(20, TimeUnit.SECONDS)
-                .build();
-        Request.Builder builder = new Request.Builder()
-                .url(url);
-        if (headerMap != null) {
-            for (String key : headerMap.keySet()) {
-                builder.header(key, headerMap.get(key));
-            }
-        }
-        try {
-            Response response = client.newCall(builder.build()).execute();
-            if (response.isSuccessful()) {
-                ResponseBody body = response.body();
-                if (body != null) {
-                    return body.string();
-                }
-            }
-            return "";
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         return "";
     }
 
